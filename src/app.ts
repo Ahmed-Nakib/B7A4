e76d485bf7273a524c6c86c1e0d2c3fdeab74cfe
Ahmed-Nakib <office.nakib@gmail.com>
2026-07-07 04:58:51 +0200

    check technician

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
-import { userRoutes } from "./modules/user/user.route";
 import { AuthRoutes } from "./modules/Auth/auth.route";
 import { CategoryRoutes } from "./modules/category/category.route";
 import { TechnicianRoutes } from "./modules/Technician/technician.route";
+import { BookingRoutes } from "./modules/booking/booking.route";
+import { ServiceRoutes } from "./modules/service/service.route";
+import { PaymentRoutes } from "./modules/payment/payment.route";
+import { AdminRoutes } from "./modules/Admin/admin.route";
+import { ReviewRoutes } from "./modules/review/review.route";
 
 
 const app : Application = express();
@@ -21,9 +25,13 @@ app.use(cors({
 }))
 
 
-app.use("/api/users", userRoutes)
 app.use("/api/auth", AuthRoutes)
 app.use("/api/categories", CategoryRoutes)
 app.use("/api/technician", TechnicianRoutes)
+app.use("/api/booking", BookingRoutes)
+app.use("/api/service", ServiceRoutes)
+app.use("/api/payment", PaymentRoutes)
+app.use("/api/admin", AdminRoutes)
+app.use("/api/review", ReviewRoutes)
 
 export default app;
